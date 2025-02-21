8bf976b065e61de93c00305fe576208232d49afe
GaganTantwar <panthigagan478@gmail.com>
2025-02-21 23:44:08 +0530

    Gagan Tantwar Extended feat: REST Call to show Hello Mark from BridgeLabz

--- HelloMessagingApp/src/main/java/com/hellomessagingapplication/controller/HelloMessage.java
+++ HelloMessagingApp/src/main/java/com/hellomessagingapplication/controller/HelloMessage.java
@@ -23,4 +23,8 @@ public class HelloMessage {
     public String deleteHello() {
         return "Hello from BridgeLabz (DELETE)";
     }
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
 }
