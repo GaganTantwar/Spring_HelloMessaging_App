1d73954d43a4c8b46e5f682044b7b9c686ab3fc3
GaganTantwar <114756520+GaganTantwar@users.noreply.github.com>
2025-02-25 11:39:30 +0530

    Gagan Tantwar fix : HelloMessage.java

--- HelloMessagingApp/src/main/java/com/hellomessagingapplication/controller/HelloMessage.java
+++ HelloMessagingApp/src/main/java/com/hellomessagingapplication/controller/HelloMessage.java
@@ -8,21 +8,6 @@ public class HelloMessage {
     public String getHello() {
         return "Hello from BridgeLabz (GET)";
     }
-
-    @PostMapping("/post")
-    public String postHello() {
-        return "Hello from BridgeLabz (POST)";
-    }
-
-    @PutMapping("/put")
-    public String putHello() {
-        return "Hello from BridgeLabz (PUT)";
-    }
-
-    @DeleteMapping("/delete")
-    public String deleteHello() {
-        return "Hello from BridgeLabz (DELETE)";
-    }
     @GetMapping("/query")
     public String sayHelloWithName(@RequestParam(value = "name", defaultValue = "World") String name) {
         return "Hello " + name + " from BridgeLabz";
